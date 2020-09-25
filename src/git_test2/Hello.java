2e5841985ae83e3971a6ffb3ce751af55432c88d
Kim, Bomi <a99150316@gmail.com>
2020-09-25 23:47:31 +0900

    오류해결

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,13 +1,5 @@
 package git_test2;
 public class Hello {
-<<<<<<< HEAD
-    
-    String message = "¾È³çÇÏ¼¼¿ä";
-
-    public String say() {
-        return message;
-=======
-
     String message;
 
     public Hello() {
@@ -16,10 +8,7 @@ public class Hello {
     public Hello(String message) {
         this.message = message;
     }
-
-    public void say() {
-        System.out.println(message);
->>>>>>> 75ac5b177980105d8cdb98df6a61c86e2a8e237b
+    public String say() {
+        return message;
     }
-
 }
